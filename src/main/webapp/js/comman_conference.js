dc0a3869dcbddc5ac33c10cdae4d5b65e3c267f7
rigel <rigel.ferreira@hotmail.com>
2017-05-09 22:35:29 -0300

    counting commands

--- src/main/webapp/js/comman_conference.js
+++ src/main/webapp/js/comman_conference.js
@@ -17,11 +17,15 @@ $(function(){
     $("#payment_type").val(gup('payment_type')).change();
   });
   var total = 0.0;
+  var count = 0;
   $("#in_commands tr").each(function(i, item){
-    if(i > 0)
+    if(i > 0) {
       total += parseFloat($($(item).find('td').get(8)).text());
+      count += 1;
+    }
   });
   $("#total").val(total.formatMoney());
+  $("#count").val(count);
   getCashiers();
   $("#detail_print").click(function(){
     window.open('/financial_cashier/comman_conference_print?'+$("#form_search :input").serialize(),'print_window_command')
