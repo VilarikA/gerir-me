51db01b56a8b57ded44040d53605a447de8d3754
rigel <rigel.ferreira@hotmail.com>
2017-06-08 17:26:00 -0300

    company add - from validations

--- src/main/webapp/js/company_add_jquery.js
+++ src/main/webapp/js/company_add_jquery.js
@@ -2,20 +2,20 @@ $(function(){
 	$("#companyAddForm").submit(function(){
 		var message = "";
 		var companyValidate = CompanyValidations.validate($('#contact').val(), $('#email').val(), $('#phone').val(), $("#name").val());
+		if(!companyValidate.companyNameLength){
+			message  += 'Nome da empresa não pode conter menos de 4 caracteres!!\n\n';
+		}
 		if(!companyValidate.contact){
-			message  += 'Existem caracteres especiais no nome do contato, remova-os antes de continuar!\n';
+			message  += 'Existem caracteres especiais no nome do contato, remova-os antes de continuar!\n\n';
 		}
 		if(!companyValidate.contactLength){
-			message  += 'Contato não pode conter menos de 4 caracteres!!\n';
-		}
-		if(!companyValidate.companyNameLength){
-			message  += 'Nome da empresa não pode conter menos de 4 caracteres!!\n';
+			message  += 'Contato não pode conter menos de 4 caracteres!!\n\n';
 		}
 		if(!companyValidate.email){
-			message += 'E-mail inválido!\n';
+			message += 'E-mail inválido!\n\n';
 		}
 		if(!companyValidate.phone){
-			message += 'Telefone inválido!\n';
+			message += 'Telefone inválido!\n\n';
 		}
 		if(message){
 			alert(message);
